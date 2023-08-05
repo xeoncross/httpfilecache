0eab6479a5d4fc2dc4eb9e545b3abf99b3d293d4
xeoncross <david@xeoncross.com>
2023-08-05 13:47:58 -0500

    updated unit test

--- httpfilecache_test.go
+++ httpfilecache_test.go
@@ -6,17 +6,21 @@ import (
 	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestCache(t *testing.T) {
 
+	var calls int32 = 0
+
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case "/404":
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		default:
+			atomic.AddInt32(&calls, 1)
 			w.Write([]byte("Hello World"))
 		}
 	}))
@@ -27,7 +31,7 @@ func TestCache(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to create test directory: %s", err)
 	}
-	// t.Log(dir)
+	t.Log(dir)
 	defer os.RemoveAll(dir)
 
 	client := NewClient(dir, time.Second*10)
@@ -48,7 +52,7 @@ func TestCache(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Cached result
+	// Cache the result
 	resp, err = client.Get(server.URL + "/")
 	if err != nil {
 		t.Fatal(err)
@@ -62,6 +66,23 @@ func TestCache(t *testing.T) {
 	_, err = os.Stat(filepath.Join(dir, path))
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	// Use cached result
+	for i := 0; i < 10; i++ {
+		resp, err = client.Get(server.URL + "/")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != 200 {
+			t.Fatalf("want: 200, got: %d\n", resp.StatusCode)
+		}
+	}
+
+	// Only the first call should have hit the server
+	if atomic.LoadInt32(&calls) != 1 {
+		t.Fatalf("Expected cache to return result, but it did not")
 	}
 }
 
